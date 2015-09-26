818aa780b8f6cf709797fb70bbda50260194bc22
aekkapun <aekkapuncs@gmail.com>
2015-09-27 01:50:16 +0700

    change menu

--- backend/themes/adminlte/layouts/menu/permitapp.php
+++ backend/themes/adminlte/layouts/menu/permitapp.php
@@ -6,13 +6,13 @@
 	      if(Yii::$app->user->can('/permitapp/default/index')) {
 	?>
             <li>
-		<?= Html::a('<i class="fa fa-angle-double-right"></i> ยื่นคำขอ',['/permitapp/app-car/create'])  ?>
+		<?= Html::a('<i class="fa fa-angle-double-right"></i> ยื่นคำขอ',['/permitapp/zform/create'])  ?>
 	    </li>
 	<?php 
 	      }
-	      if(Yii::$app->user->can('/admin/permission/index')) { ?>
+	       if(Yii::$app->user->can('/permitapp/default/index')) {?>
             <li>
-		<?= Html::a('<i class="fa fa-angle-double-right"></i> Permission',['/admin/permission'])  ?>
+		<?= Html::a('<i class="fa fa-angle-double-right"></i> ราการคำขอ',['/permitapp/zform/index'])  ?>
 	    </li>
 	<?php }
 	      if(Yii::$app->user->can('/admin/role/index')) {
